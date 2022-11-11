0777452ec7822fe76e4830b47bac31fc5553734e
benardop <benard.oc.p@gmail.com>
2022-11-11 18:50:05 +0300

    Add main.rb class

--- main.rb
+++ main.rb
@@ -1,7 +1,41 @@
-require_relative 'app'
+require_relative './app'
+require_relative './display_menu'
+require_relative './create_file_dir'
 
 def main
   app = App.new
-  app.run
+  Dir.exist?('./data') ? app.read_files : nil
+  loop do
+    display_menu
+    choose_action(app)
+  end
 end
+
+def choose_action(app)
+  choice = gets.chomp
+  case choice
+  when '1'
+    app.display_books
+  when '2'
+    app.display_people
+  when '3'
+    app.create_person
+  when '4'
+    app.create_book
+  when '5'
+    app.create_rental
+  when '6'
+    app.display_rental
+  else
+    exit_app(app)
+  end
+end
+
+def exit_app(app)
+  puts 'Thank you for using this app! Now exiting...'
+  create_dir
+  app.save_files
+  exit
+end
+
 main
