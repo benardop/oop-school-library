f1522c641d5e5a2a2277d431c2b4d764a4b0a23d
benardop <benard.oc.p@gmail.com>
2022-11-11 23:45:48 +0300

    Update capitalize_decorator.rb file

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -1,7 +1,6 @@
-require_relative '/.decorator'
-
-class CapitalizeDecorator < Decorator
+require_relative 'base_decorator'
+class CapitalizeDecorator < BaseDecorator
   def correct_name
-    @nameable.correct_name.capitalize
+    @nameable.correct_name.upcase
   end
 end
