baeee9ff10772b0895f0835a68fde82ab6e9087f
John <kibirigejohn005@gmail.com>
2022-11-18 16:11:30 +0300

    create specs for students

---
+++ spec/student_spec.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+require './student'
+require './classroom.rb'
+
+student = Student.new('p4', 10, 'Lisa', true)
+clss = Classroom.new('e')
+
+RSpec.describe 'Student' do 
+    it "ensure that we are able to access all the properties that are accessible" do 
+        expect(student.name).to eq('Lisa')
+        expect(student.classroom).to eq('p4')
+        expect(student.type).to eq('Student')
+        expect(student.age).to be(10)
+        expect(student.parent_permission).to be_truthy
+    end
+
+    it 'student plays hooky' do 
+        expect(student.play_hooky).to eq('¯(ツ)/¯')
+    end
+
+    it 'add_classroom method' do 
+        expect(student.add_classroom(clss)).to be_truthy
+    end
+
+end
